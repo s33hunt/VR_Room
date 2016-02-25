b454b74ed49dcd0308f866304d0f73f4fc924b4c
charlie <s33hunt@gmail.com>
2016-02-25 10:45:34 -0600

    helm grabbable and wearable

---
+++ orion/Assets/tests/test room/HelmController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmController : MonoBehaviour {
+	public float equipDistance = .4f;
+	public GrabbableObject grabob;
+	public Transform hmd;
+	public GameObject headHelm;
+	bool isGrabbed { get { return grabob.IsGrabbed(); } }
+	bool helmOnHead = false;
+
+	void Start() { headHelm.SetActive(false); }
+
+	void Update () {
+		if(isGrabbed && Vector3.Distance(transform.position, hmd.position) < equipDistance && !helmOnHead) {
+			helmOnHead = true;
+			headHelm.SetActive(true);
+			HMDAlignmentTest.instance.ascii.enabled = true;
+			Destroy(gameObject);
+		}
+	}
+}
